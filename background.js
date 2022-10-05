a54edcbc675629eb492713f997f62d7a0aadcc09
Tunnes <cauepani06@gmail.com>
2022-10-05 15:19:55 -0300

    add more files

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.tabs.create({ 'url': "https://alihunter.io/product-analysis"})
+// chrome.runtime.onInstalled.addListener((infos) => {
+//   chrome.tabs.create({ 'url': "https://alihunter.io/product-analysis"})
 
-})
+// })
 
 let html = document.documentElement.innerHTML;
 
 if (html.includes("shopify")) {
-  $("body").append("<div class='wrapper-alihunter'></div>")
+  $("body").append("<div id='wrapper-alihunter'></div>")
+  $("#wrapper-alihunter").append("<img src='alihunter-logo.png' />")
 }
